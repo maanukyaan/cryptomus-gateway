ef822a8dd82072b33a626899d949b40583f16244
Vahe <8384838z@gmail.com>
2023-10-07 23:16:46 +0300

    Files deleted

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Nav from "./../Nav/Nav";
 import Header from "../Header/Header";
--- database/database.service.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export class DatabaseService extends PrismaClient {
-  async init() {
-    await this.$connect();
-  }
-}
