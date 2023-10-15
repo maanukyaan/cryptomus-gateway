fb7ecd0b07a81753ecfe8e91c6d2bdcc86392172
Vahe <8384838z@gmail.com>
2023-10-15 19:10:58 +0300

    configuring server

--- client/src/components/Categories/CategoryContainer/CategoryContainer.jsx
+++ client/src/components/Categories/CategoryContainer/CategoryContainer.jsx
@@ -17,7 +17,7 @@ export default function CategoryContainer() {
   const fetchData = async (link) => {
     try {
       const response = await axios.get(
-        `http://localhost:5000/api/getCategories/${link}`
+        `http://154.7.253.78:5000/api/getCategories/${link}`
       );
       console.log(response.data);
       setCategoryData(response.data);
--- server.js
+++ server.js
@@ -33,8 +33,11 @@ async function run() {
     app.use((req, res, next) => {
       const allowedOrigins = [
         "http://localhost:3000",
-        "https://154.7.253.78",
-        "https://valgoshop.com",
+        "http://localhost:5000",
+        "http://154.7.253.78:5000",
+        "http://154.7.253.78",
+        "https://accspalace.com",
+        "https://api.accspalace.com",
       ]; // Список разрешенных IP-адресов
       const origin = req.headers.origin;
       if (allowedOrigins.includes(origin)) {
